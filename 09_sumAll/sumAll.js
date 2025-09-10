0d6b97bad5bd9790b23c0a7dd4158e9c56c83b9c
Saeed Habibi <178019482+saeed-habibi@users.noreply.github.com>
2025-09-10 16:04:03 +0330

    Solve 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+	if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+	if (a < 0 || b < 0) return "ERROR";
 
+	let firstNum;
+	let lastNum;
+
+	if (a < b) {
+		firstNum = a;
+		lastNum = b;
+	} else {
+		firstNum = b;
+		lastNum = a;
+	}
+
+	const nums = [];
+
+	for (let i = firstNum; i <= lastNum; i++) {
+		nums.push(i);
+	}
+
+	const sum = nums.reduce((total, current) => total + current, 0);
+
+	return sum;
 };
 
 // Do not edit below this line
