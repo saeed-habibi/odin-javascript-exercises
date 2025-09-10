07636cc1b5dc3edaa33f362d344c54e9fa0d0d83
Saeed Habibi <178019482+saeed-habibi@users.noreply.github.com>
2025-09-10 16:27:02 +0330

    Solve 11_tempConversions

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  // C = (F - 32) * 5/9
+  const tempInCelsius = (tempInFahrenheit - 32) * (5/9);
+  const rounded = Math.round(tempInCelsius * 10) / 10;
+
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  // F = (C * 9/5) + 32
+  const tempInFahrenheit = (tempInCelsius * (9/5)) + 32;
+  const rounded = Math.round(tempInFahrenheit * 10) / 10;
+
+  return rounded;
 };
 
 // Do not edit below this line
