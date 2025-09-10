a2c67a3869d83a785a0a0e4bb64b11be80d7c9e9
Saeed Habibi <178019482+saeed-habibi@users.noreply.github.com>
2025-09-10 15:46:31 +0330

    Solve 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+	for (let i = arr.length - 1; i >= 0; i--) {
+		const shouldRemove = toRemove.includes(arr[i]);
+
+		if (shouldRemove) arr.splice(i, 1);
+	}
+
+	return arr;
 };
 
 // Do not edit below this line
