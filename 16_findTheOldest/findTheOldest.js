78e864a734a05439c9765ad55e352087887f3f30
Saeed Habibi <178019482+saeed-habibi@users.noreply.github.com>
2025-09-19 17:49:19 +0330

    Solve 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	people.sort((a, b) => {
+		const ageOfA = a.yearOfDeath ?
+			a.yearOfDeath - a.yearOfBirth : 
+			new Date().getFullYear() - a.yearOfBirth;
 
+		const ageOfB = b.yearOfDeath ?
+			b.yearOfDeath - b.yearOfBirth :
+			new Date().getFullYear() - b.yearOfBirth;
+
+		return ageOfA - ageOfB;
+	});
+
+	return people.pop();
 };
 
 // Do not edit below this line
