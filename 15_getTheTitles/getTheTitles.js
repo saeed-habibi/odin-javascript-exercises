63f4e63f9e9675cbb0330e5775eb35a027591659
Saeed Habibi <178019482+saeed-habibi@users.noreply.github.com>
2025-09-19 17:31:43 +0330

    Solve 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(input) {
+	return input.reduce((output, currentBook) => {
+		output.push(currentBook.title);
+		return output;
+	}, []);
 };
 
 // Do not edit below this line
