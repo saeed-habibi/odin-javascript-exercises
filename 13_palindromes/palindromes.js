e39c97819e10125169eb982a6c10c55cb5a17674
Saeed Habibi <178019482+saeed-habibi@users.noreply.github.com>
2025-09-19 17:01:52 +0330

    Solve 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const cleanArr = str.match(/\w/g);
+	const cleanStr = cleanArr.join("").toLowerCase();
+	const reversedStr = cleanArr.reverse().join("").toLowerCase();
 
+	if (cleanStr === reversedStr) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
