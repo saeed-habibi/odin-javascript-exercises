d7a0bd613acfc70b0afaa08927f0863340f5e28f
Saeed Habibi <178019482+saeed-habibi@users.noreply.github.com>
2025-09-19 17:25:10 +0330

    Solve 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+	const num = Number(input);
+	if (num === 0) return 0;
+	if (num === 1) return 1;
+	if (num < 0) return "OOPS";
 
+	const fibSeq = [0, 1];
+
+	for (let i = 2; i <= num; i++) {
+		const nextNumber = fibSeq.at(-1) + fibSeq.at(-2);
+		fibSeq.push(nextNumber);
+	}
+
+	return fibSeq.pop();
 };
 
 // Do not edit below this line
